aea566c0d53e012842eee4c35ddcae6334ce4d1d
kennyzlei <kennyzlei@gmail.com>
2016-04-13 15:45:46 -0400

    Remove incompatible jQuery UI stuff

--- app.js
+++ app.js
@@ -158,11 +158,6 @@ var checkPage = function(){
     if($("#guest-reservations").length) {
         loadGuestReservations();
     }
-    
-    $( "#hello" ).dialog({ autoOpen: false });
-    $( ".say_it" ).click(function() {
-    $( "#hello" ).dialog( "open" );
-    });
 };
 
 
